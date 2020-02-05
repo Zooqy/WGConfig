2bbd1602b017f3e94fe78b6f6d343610cd7de14a
hayden robinson <hayden2911@hotmail.co.uk>
2020-02-05 21:01:14 +0000

    Genarated three key funtions to meet the goal of the program and call for each of the three functions

--- wgconfig.php
+++ wgconfig.php
@@ -1,31 +1,37 @@
 #!/usr/bin/php
 <?php
 
-echo "Test123".PHP_EOL;
-echo "[Interface]".PHP_EOL;
-echo "Address =".PHP_EOL;   
-echo "PrivateKey".PHP_EOL;
+function ClientConfig($strInterfaceAddress, $strPrivateKey, $strPublicKey, $strEndPoint, $strEndPointPort, $strAllowedIPs) {
+    echo "[Interface]".PHP_EOL;
+    echo "Address = ".$strInterfaceAddress.PHP_EOL;   
+    echo "PrivateKey = ".$strPrivateKey.PHP_EOL;
 
-echo "[Peer]".PHP_EOL;
-echo "PublicKey".PHP_EOL;
-echo "Endpoint".PHP_EOL;
-echo "AllowIPs".PHP_EOL;
-echo "PersistentKeepalive = 21".PHP_EOL;
+    echo "[Peer]".PHP_EOL;
+    echo "PublicKey = ".$strPublicKey.PHP_EOL;
+    echo "Endpoint = ".$strEndPoint.":".$strEndPointPort.PHP_EOL;
+    echo "AllowIPs = ".$strAllowedIPs.PHP_EOL;
+    echo "PersistentKeepalive = 21".PHP_EOL;
+}
 
-echo "[Interface]".PHP_EOL;
-echo "Address".PHP_EOL;
-echo "PostUp".PHP_EOL;
-echo "PostDown".PHP_EOL;
-echo "ListenPort".PHP_EOL;
-echo "PrivateKey".PHP_EOL;
+function ServerInterface($strInterfaceAddress, $strNetworkAdapter, $strListenPort, $strPrivateKey) {
+    echo "[Interface]".PHP_EOL;
+    echo "Address = ".$strInterfaceAddress.PHP_EOL;
+    echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o ".$strNetworkAdapter." -j MASQUERADE ".PHP_EOL;
+    echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ".$strNetworkAdapter."  -j MASQUERADE ".PHP_EOL;
+    echo "ListenPort = ",$strListenPort.PHP_EOL;
+    echo "PrivateKey = ".$strPrivateKey.PHP_EOL;
+}
+function ServerPeer($strPublicKey, $strAllowedIPs) {
+    echo "[Peer]".PHP_EOL;
+    echo "PublicKey = ".$strPublicKey.PHP_EOL;
+    echo "AllowedIPs = ".$strAllowedIPs.PHP_EOL;
+    echo "PersistentKeepalive = 21".PHP_EOL;
+}
 
-echo "[Peer]".PHP_EOL;
-echo "PublicKey".PHP_EOL;
-echo "AllowedIPs".PHP_EOL;
-echo "PersistemtKeepalive = 21".PHP_EOL;
 
-
-
+//ClientConfig(" Test1", "Test2", "Test3", "Test4", "Test5", "Test6");
+//ServerInterface(" Test1", "Test2", "Test3", "Test4", "Test5");
+//ServerPeer(" Test1", "Test2");
 
 
 ?>
