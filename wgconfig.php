e648318cfbdc5307279cdbd22cdbfc52ac27c83c
hayden robinson <hayden2911@hotmail.co.uk>
2020-02-05 19:48:34 +0000

    First commit

---
+++ wgconfig.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+echo "Test123".PHP_EOL;
+echo "[Interface]".PHP_EOL;
+echo "Address =".PHP_EOL;   
+echo "PrivateKey".PHP_EOL;
+
+echo "[Peer]".PHP_EOL;
+echo "PublicKey".PHP_EOL;
+echo "Endpoint".PHP_EOL;
+echo "AllowIPs".PHP_EOL;
+echo "PersistentKeepalive = 21".PHP_EOL;
+
+echo "[Interface]".PHP_EOL;
+echo "Address".PHP_EOL;
+echo "PostUp".PHP_EOL;
+echo "PostDown".PHP_EOL;
+echo "ListenPort".PHP_EOL;
+echo "PrivateKey".PHP_EOL;
+
+echo "[Peer]".PHP_EOL;
+echo "PublicKey".PHP_EOL;
+echo "AllowedIPs".PHP_EOL;
+echo "PersistemtKeepalive = 21".PHP_EOL;
+
+
+
+
+
+?>
