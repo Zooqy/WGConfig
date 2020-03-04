c9b61ea8aa1fd04b76d86949a8d5fb2158e0fb32
hayden robinson <hayden2911@hotmail.co.uk>
2020-03-04 20:34:35 +0000

    Started to add command line options

--- wgconfig.php
+++ wgconfig.php
@@ -29,9 +29,42 @@ function ServerPeer($strPublicKey, $strAllowedIPs) {
 }
 
 
+//main program starts here
+
 //ClientConfig(" Test1", "Test2", "Test3", "Test4", "Test5", "Test6");
 //ServerInterface(" Test1", "Test2", "Test3", "Test4", "Test5");
 //ServerPeer(" Test1", "Test2");
+$shortopts="";
+$shortopts .= "h::";
+$shortopts .= "s::";
+$shortopts .= "c::";
+$longopts  = array(
+    "address::",       // This is for the server address
+    "listenport::",    // This is the server Port 
+    "privatekey::",    // This is for private keys
+    "allowedips::",    // This is to show allowedip's
+    "publickey::",     // This is to show the public key
+    "nic::",           // This is to show the network interface card
+    "endpoint::"       // This is to show the target host 
+);
+$options = getopt($shortopts, $longopts);
+
+var_dump($options);
+
+$clientmode=0;
+$servermode=0;
+
+if (array_key_exists('c',$options)) {
+        echo "-c was specified.  innit. ".PHP_EOL;
+        $clientmode=1;
+}
 
 
-?>
+if (array_key_exists('s',$options)) {
+    echo "-s was specified.  innit. ".PHP_EOL;
+    $servermode=1;
+}
+
+$total=$clientmode+$servermode
+
+?> 
